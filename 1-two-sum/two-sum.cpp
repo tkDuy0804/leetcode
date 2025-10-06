846e391ac86ce3193ce35184f7b351b0ae507acf
tkDuy0804 <102163627+tkDuy0804@users.noreply.github.com>
2025-10-06 18:40:01 +0700

    Time: 0 ms (100.00%) | Memory: 14.9 MB (19.90%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> prevVal;
+        vector<int> indices;
+
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int diff = target - nums.at(i);
+            auto it = prevVal.find(diff);
+
+            if(it != prevVal.end())
+            {
+                indices.push_back(prevVal.at(diff));
+                indices.push_back(i);
+                return indices;
+            }
+
+            prevVal[nums.at(i)] = i;
+        }
+
+        return vector<int>{};
+    }
+};
