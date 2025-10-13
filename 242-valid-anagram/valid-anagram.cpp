ae4976d21fb464122dc19101d5985727f99812a2
tkDuy0804 <102163627+tkDuy0804@users.noreply.github.com>
2025-10-13 22:42:16 +0700

    Time: 0 ms (100.00%) | Memory: 10 MB (11.26%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length())
+    unordered_map<char, int> countS, countT;
+
+    if(s.length() != t.length())
+    {
+        return false;
+    }
+
+    for(int i = 0; i < s.length(); i++)
+    {
+        countS[s.at(i)] = countS[s.at(i)] + 1;
+        countT[t.at(i)] = countT[t.at(i)] + 1;
+    }
+
+    for(auto &pair : countS)
+    {
+        char c = pair.first;
+        if(countS[c] != countT[c])
         {
             return false;
         }
+    }
 
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-
-        if(s.compare(t))
-        {
-            return false;
-        }
-
-        return true;
+    return true;
     }
 };
