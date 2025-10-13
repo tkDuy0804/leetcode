9900fbc8a6b69242370009d32984fe8d89436f1a
tkDuy0804 <102163627+tkDuy0804@users.noreply.github.com>
2025-10-13 22:46:10 +0700

    Time: 8 ms (8.11%) | Memory: 9.8 MB (48.29%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length())
-        {
-            return false;
-        }
 
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
